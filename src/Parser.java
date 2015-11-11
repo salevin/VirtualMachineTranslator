9632acda84b056e140ecc599f5c497065ec92500
oggz <adoccolo@gmail.com>
2015-11-11 14:44:31 -0500

    parser near complete

--- src/Parser.java
+++ src/Parser.java
@@ -5,51 +5,109 @@ import java.util.Scanner;
 
 public class Parser {
 
-	// current is the current command, i.e., the command that the Parser
-	// should act on.
-	private String current, next;
-	private Scanner scanner;
-	private boolean hasMoreCommands;
+    // current is the current command, i.e., the command that the Parser
+    // should act on.
+    private String current, next;
+    private Scanner scanner;
+    private boolean hasMoreCommands;
 
-	public Parser(File src) {
-		hasMoreCommands = true;
+    public Parser(File src) {
+        hasMoreCommands = true;
 
-		try {
-			scanner = new Scanner(src);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+        try {
+            scanner = new Scanner(src);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
-		current = "";
-		next = getNextCommand();
-	}
+        current = "";
+        next = getNextCommand();
+    }
 
-	public boolean hasMoreCommands() {
-		return hasMoreCommands;
-	}
+    public int commandType() {
+        if (current.subString(0, current.indexOf(" ") == "function")) {
+            return C_FUNCTION;
+        }
+        else if (current.subString(0, current.indexOf(" ") == "push")) {
+            return C_PUSH;
+        }
+        else if (current.subString(0, current.indexOf(" ") == "pop")) {
+            return C_POP;
+        }
+        else if (current.subString(0, current.indexOf(" ") == "label")) {
+            return C_LABEL;
+        }
+        else if (current.subString(0, current.indexOf(" ") == "return")) {
+            return C_RETURN;
+        }
+        else if (current.subString(0, current.indexOf(" ") == "call")) {
+            return C_CALL;
+        }
+        else if (current.subString(0, current.indexOf(" ") == "goto")) {
+            return C_goto;
+        }
+        else if (current.subString(0, current.indexOf(" ") == "if-goto")) {
+            return C_IF;
+        }
+        else {
+            return C_ARITHMETIC;
+        }
+    }
+            
+                
+    public String arg1() {
+        if (commandType() == C_ARITHMETIC) {
+            return current
+        }
+        else if (commandType() == C_RETURN) {
+            return null;
+        }
+        else {
+            int i = current.indexOf(" ");
+            int j = current.indexOf(" ", i);
+            return current.subString(i++, j);
+        }
+    }
 
-	public void advance() {
-		current = next;
-		next = getNextCommand();
-	}
+    // possibly need to fix later
+    public int arg2() {
+        if (commandType() == C_POP || commandType() == C_PUSH || commandType() == C_FUNCTION || commandType() == C_CALL) {
+            int i = current.indexOf(" ");
+            int j = current.indexOf(" ", i);
+            int k = current.indexOf(" ", j);
+            return current.subString(j++, k);
+        }
+        else {
+            return null;
+        }
+    }
 
-	private String getNextCommand() {
-		String next;
+    public boolean hasMoreCommands() {
+        return hasMoreCommands;
+    }
 
-		// The following regular expression matches any line that is all
-		// whitespace or is only a comment.  Continue scanning the input
-		// file until we get an actual command or empty the file.
+    public void advance() {
+        current = next;
+        next = getNextCommand();
+    }
 
-		do {
-			if (scanner.hasNextLine())
-				next = scanner.nextLine();
-			else {
-				hasMoreCommands = false;
-				return "";
-			}
-		} while (next.matches("(^\\s*$)|(^\\s*//.*$)"));
+    private String getNextCommand() {
+        String next;
 
-		return next;
-	}
+        // The following regular expression matches any line that is all
+        // whitespace or is only a comment.  Continue scanning the input
+        // file until we get an actual command or empty the file.
+
+        do {
+            if (scanner.hasNextLine())
+                next = scanner.nextLine();
+            else {
+                hasMoreCommands = false;
+                return "";
+            }
+        } while (next.matches("(^\\s*$)|(^\\s*//.*$)"));
+
+        return next;
+    }
 }
