73033a5d2bed01f18b5e59de882ffde28029975d
oggz <adoccolo@gmail.com>
2015-12-04 16:44:07 -0500

    minor changes

--- src/CodeWriter.java
+++ src/CodeWriter.java
@@ -69,7 +69,51 @@ public class CodeWriter {
     }
 
     public void writeReturn() {
-
+        "@LCL\n" +
+            "D=A\n" +
+            "@R14\n" +
+            "A=D\n" +
+            // ret
+            "@R14\n" +
+            "D=A\n" +
+            "@5\n" +
+            "D=D-A\n" +
+            "@R15\n" +
+            "A=D\n" +
+            //pop shit
+            //TODO
+            //sp
+            "@ARG\n" +
+            "D=M+1\n" +
+            "@SP\n" +
+            "M=D+A\n" +
+            //that
+            "@R14\n" +
+            "D=A-1\n" +
+            "@THAT\n" +
+            "M=D\n" +
+            //this
+            "@R14\n" +
+            "D=A\n" +
+            "@2\n" +
+            "D=D-A\n" +
+            "@THAT\n" +
+            "M=D\n" +
+            //arg
+            "@R14\n" +
+            "D=A\n" +
+            "@3\n" +
+            "D=D-A\n" +
+            "@ARG\n" +
+            "M=D\n" +
+            //lcl
+            "@R14\n" +
+            "D=A\n" +
+            "@4\n" +
+            "D=D-A\n" +
+            "@LCL\n" +
+            "M=D\n";
+        writeGoTo("R15")
     }
 
     public void writeFunction(String functionName, Integer numLocals) {
