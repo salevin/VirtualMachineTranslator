861d6d379e3e46aeeb0c50f28e0a43a4ac853888
sam <samueljlevin@gmail.com>
2015-12-07 13:57:45 -0500

    Minor fixes

--- src/CodeWriter.java
+++ src/CodeWriter.java
@@ -69,56 +69,8 @@ public class CodeWriter {
     }
 
     public void writeReturn() {
-        "@LCL\n" +
-            "D=A\n" +
-            "@R14\n" +
-            "A=D\n" +
-            // ret
-            "@R14\n" +
-            "D=A\n" +
-            "@5\n" +
-            "D=D-A\n" +
-            "@R15\n" +
-            "A=D\n" +
-            //pop shit
-            "@SP\n" +
-            "AM=M-1\n" +
-            "D=M\n" +
-            "@ARG\n" +
-            "A=M\n" +
-            "M=D\n" +
-            //sp
-            "@ARG\n" +
-            "D=M+1\n" +
-            "@SP\n" +
-            "M=D+A\n" +
-            //that
-            "@R14\n" +
-            "D=A-1\n" +
-            "@THAT\n" +
-            "M=D\n" +
-            //this
-            "@R14\n" +
-            "D=A\n" +
-            "@2\n" +
-            "D=D-A\n" +
-            "@THAT\n" +
-            "M=D\n" +
-            //arg
-            "@R14\n" +
-            "D=A\n" +
-            "@3\n" +
-            "D=D-A\n" +
-            "@ARG\n" +
-            "M=D\n" +
-            //lcl
-            "@R14\n" +
-            "D=A\n" +
-            "@4\n" +
-            "D=D-A\n" +
-            "@LCL\n" +
-            "M=D\n";
-        writeGoTo("R15")
+       writeCode(ReturnWriter.RETURN);
+        writeGoTo("R15");
     }
 
     public void writeFunction(String functionName, Integer numLocals) {
